9d8edcf0a7d626acc05764ebaa2e08bbca09084c
Jason Nutt <jnutt367@gmail.com>
2021-04-24 20:22:36 -0400

    commit with small scall animations

--- gsap.js
+++ gsap.js
@@ -2,14 +2,14 @@ gsap.registerPlugin(ScrollTrigger)
 
 let tl = gsap.timeline({
         scrollTrigger: {
-            trigger: "",
+            trigger: "#dino-days",
             markers: true,
             pin: true,
             start: "top 60%",
             end: "bottom 40%",
-
+            toggleActions: "play none reverse play"
         }
     })
-    .fromTo("#", { opacity: 0 }, { opacity: 1 })
-    .fromTo("", { opacity: 0 }, { opacity: 1 })
-    .fromTo("", { opacity: 0 }, { opacity: 1 })
+    .fromTo("#hourglass", { opacity: 0 }, { duration: 3, rotate: 360, opacity: 1 })
+    .fromTo("#first-cloud", { opacity: 0 }, { duration: 2, opacity: 1, ease: "elastic", scale: "1.1", repeat: -1 })
+    .fromTo("#second-cloud", { opacity: 0 }, { duration: 2, opacity: 1, ease: "elastic", scale: "1.1", repeat: -1 })
