cb015a5bbf8decea73758f94d7d72d0c0d1b2e5f
Jason Nutt <jnutt367@gmail.com>
2021-04-25 08:38:14 -0400

    commit got rid of black bg and working down slowly implementing layer by layer

--- gsap.js
+++ gsap.js
@@ -6,10 +6,11 @@ let tl = gsap.timeline({
             markers: true,
             pin: true,
             start: "top 60%",
-            end: "bottom 40%",
+            end: "bottom 55%",
             toggleActions: "play none reverse play"
         }
     })
+    .fromTo("#dino-days", { x: -300, opacity: 0 }, { duration: 2.5, opacity: 1, x: 10, ease: "elastic" })
     .fromTo("#hourglass", { opacity: 0 }, { duration: 3, rotate: 360, opacity: 1 })
     .fromTo("#first-cloud", { opacity: 0 }, { duration: 2, opacity: 1, ease: "back", scale: "1.2", repeat: -1 })
     .fromTo("#second-cloud", { opacity: 0 }, { duration: 2, opacity: 1, ease: "back", scale: "1.1", repeat: -1 })
