4926462e99f13b55194569c23294ef0288929144
Jason Nutt <jnutt367@gmail.com>
2021-04-27 13:18:34 -0400

    commit to remake with medea queries

--- gsap.js
+++ gsap.js
@@ -2,8 +2,7 @@ gsap.registerPlugin(ScrollTrigger)
 
 let tl = gsap.timeline({
         scrollTrigger: {
-            trigger: "#dino-days",
-            test: "second-caveman",
+            trigger: "",
             markers: false,
             pin: true,
             start: "top 75%",
@@ -13,8 +12,6 @@ let tl = gsap.timeline({
     })
     .fromTo("#dino-days", { opacity: 0 }, { duration: 2.5, fontWeight: "900", scale: "1.2", opacity: 1, ease: "elastic" })
     .fromTo("#hourglass", { opacity: 0 }, { duration: 1, rotate: 360, opacity: 1 })
-    .fromTo("#first-cloud", { opacity: 0 }, { opacity: 1, ease: "back", scale: "1.2", repeat: 3 })
-    .fromTo("#second-cloud", { opacity: 0 }, { opacity: 1, ease: "back", scale: "1.1", repeat: 3 })
     .to("#first-volcano", { opacity: 1, duration: .1, x: 1, repeat: "-1" })
     .fromTo("#first-red-bubble", { opacity: 0 }, { opacity: 1, scale: 1.2, ease: "bounce.inOut", repeat: "-1" })
     .fromTo("#first-bubble-group", { opacity: 0 }, { opacity: 1, scale: 1.2, ease: "elastic", repeat: "-1" })
