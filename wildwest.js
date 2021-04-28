e537e7076e8ece5c6c06b29c226278a27c29f94f
Jason Nutt <jnutt367@gmail.com>
2021-04-28 14:03:23 -0400

    commit for slower clouds

--- wildwest.js
+++ wildwest.js
@@ -16,5 +16,5 @@ let tl = gsap.timeline({
     .fromTo("#hourglass", { opacity: 0 }, { opacity: 1, rotate: 720 })
     .fromTo("#train", { x: 300, opacity: 1 }, { duration: 5, x: -300, opacity: 1, repeat: -1 }, 0)
     .fromTo("#smoke", { x: 300, opacity: 1 }, { duration: 5, x: -300, opacity: 1, repeat: -1 }, 0)
-    .fromTo("#clouds", { x: -300, opacity: 1 }, { x: 300, opacity: 1, repeat: -1 }, 0)
+    .fromTo("#clouds", { x: -300, opacity: 1 }, { duration: 3, x: 300, opacity: 1, repeat: -1 }, 0)
     .fromTo("#sun", { x: -40, opacity: 1 }, { duration: 5, x: 300, opacity: 1, repeat: -1 }, 0)
