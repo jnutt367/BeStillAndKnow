92ab4bc6265b22a061f0ff8bca9afbe0b598f33f
Jason Nutt <jnutt367@gmail.com>
2021-04-28 15:14:59 -0400

    commit for final product wild west

--- wildwest.js
+++ wildwest.js
@@ -14,8 +14,11 @@ let tl = gsap.timeline({
         }
     })
     .fromTo("#hourglass", { opacity: 0 }, { opacity: 1, rotate: 720 })
-    .fromTo("#train", { x: 300 }, { duration: 5, x: -300, opacity: 1, repeat: -1 }, 0)
+    .fromTo("#train", { x: 300, opacity: 1 }, { duration: 5, x: -300, opacity: 1, repeat: -1 }, 0)
     .fromTo("#smoke", { x: 300, opacity: 1 }, { duration: 5, x: -300, opacity: 1, repeat: -1 }, 0)
     .fromTo("#clouds", { x: -300, opacity: 1 }, { duration: 3, x: 300, opacity: 0.9, ease: "power2.inOut", repeat: -1 }, 0)
     .fromTo("#sun", { x: -40, opacity: 1 }, { duration: 5, x: 300, opacity: 1, repeat: -1 }, 0)
-    .to("#cow1", { x: 10 })
+    .to("#cow1", { x: -10, opacity: 1 })
+    .fromTo("#sheriff", { y: -40, opacity: 1 }, { duration: 1.5, y: 60, scale: "1.3", opacity: 1 }, 0)
+    .fromTo("#outlaw", { x: 10, opacity: 1 }, { x: 50, y: 70, opacity: 1 }, +0.5)
+    .fromTo("#draw", { opacity: 0 }, { opacity: 1 }, 1.5)
